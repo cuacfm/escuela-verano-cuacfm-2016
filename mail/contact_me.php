e1a41bb056ed70f880afcd7f2d08e0c94881044e
Jose Maria Casanova Crespo <jmcasanova@igalia.com>
2016-07-03 13:35:36 +0200

    Actualizados correos de contacto formulario escuela de verano

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'tecnoloxia@cuacfm.org'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'escoladeveran@cuacfm.org'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Correo escola de veran:  $name";
 $email_body = "Nuevo correo escola de veran.\n\n"."Esto contan:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@cuacfm.org\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: tecnoloxia@cuacfm.org\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
